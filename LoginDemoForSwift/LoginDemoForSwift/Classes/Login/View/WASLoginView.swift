1ba41d60dbf79b637d2da916e94668b96378c188
andysection <398148947@qq.com>
2017-09-06 15:39:59 +0800

    no message

---
+++ LoginDemoForSwift/LoginDemoForSwift/Classes/Login/View/WASLoginView.swift
@@ -0,0 +1,21 @@
+//
+//  WASLoginView.swift
+//  LoginDemoForSwift
+//
+//  Created by apple on 2017/9/6.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+import UIKit
+
+class WASLoginView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
